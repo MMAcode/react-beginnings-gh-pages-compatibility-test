6bda3da9767104e66b6567f0189c86f586d28dfe
MMAcode <52301171+MMAcode@users.noreply.github.com>
2020-03-22 16:46:31 +0000

    Updates

---
+++ precache-manifest.d2b581719df447b889130a4252bc1add.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03781604bfcf2ce52e3b7f020a2b04d6",
+    "url": "/react-beginnings-gh-pages-compatibility-test/index.html"
+  },
+  {
+    "revision": "0f3ddbe60b00ff27cad0",
+    "url": "/react-beginnings-gh-pages-compatibility-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7527e363c80e3992004a",
+    "url": "/react-beginnings-gh-pages-compatibility-test/static/js/2.099334c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-beginnings-gh-pages-compatibility-test/static/js/2.099334c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3ddbe60b00ff27cad0",
+    "url": "/react-beginnings-gh-pages-compatibility-test/static/js/main.f5104db4.chunk.js"
+  },
+  {
+    "revision": "048e257a70539eb4d9cc",
+    "url": "/react-beginnings-gh-pages-compatibility-test/static/js/runtime-main.99d8f9a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-beginnings-gh-pages-compatibility-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
